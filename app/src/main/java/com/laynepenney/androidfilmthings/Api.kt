1b0f220744b5bf1631a6a18f334e3bf42fd7cd04
Layne Penney <laynepenney@gmail.com>
2020-11-12 12:10:27 -0600

    add films service and test

--- app/src/main/java/com/laynepenney/androidfilmthings/Api.kt
+++ app/src/main/java/com/laynepenney/androidfilmthings/Api.kt
@@ -3,6 +3,11 @@ package com.laynepenney.androidfilmthings
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
 
 @JsonClass(generateAdapter = true)
 data class FilmsResponse(
@@ -22,12 +27,30 @@ data class Film(
 
 // TODO: dependency injection
 class Api(
-    val moshi: Moshi
+    val moshi: Moshi,
+    val retrofit: Retrofit
 ) {
+    val swapi: Swapi = retrofit.create()
+
     constructor() : this(
         moshi = Moshi.Builder()
             // TODO: any custom factories
             .addLast(KotlinJsonAdapterFactory())
             .build()
     )
+
+    constructor(
+        moshi: Moshi
+    ) : this(
+        moshi = moshi,
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://swapi.dev/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    )
+}
+
+interface Swapi {
+    @GET("films")
+    fun getFilms(): Call<FilmsResponse>
 }
--- app/src/test/java/com/laynepenney/androidfilmthings/Tests.kt
+++ app/src/test/java/com/laynepenney/androidfilmthings/Tests.kt
@@ -5,8 +5,10 @@ import okio.buffer
 import okio.source
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Response
 import java.io.File
 import java.io.FileNotFoundException
+import java.lang.NullPointerException
 
 class MoshiTest {
     lateinit var api: Api
@@ -32,8 +34,21 @@ class MoshiTest {
     fun testFilmsParsing() {
         val adapter = api.moshi.adapter<FilmsResponse>()
         println(adapter.toString())
-        val response = adapter.fromJson(filmsFile.source().buffer())!!
-        assert(response.count == 6) { "count is off" }
-        assert(response.results.size == 6) { "results size is off" }
+        val result = adapter.fromJson(filmsFile.source().buffer())!!
+        assertFilmsResult(result)
+    }
+
+    @Test
+    fun testFilmsCall() {
+        val response: Response<FilmsResponse> = api.swapi.getFilms().execute()
+        assert(response.isSuccessful) { "not successful: ${response.errorBody()}" }
+        val result: FilmsResponse = response.body()
+            ?: throw NullPointerException("response is null")
+        assertFilmsResult(result)
+    }
+
+    private fun assertFilmsResult(result: FilmsResponse) {
+        assert(result.count == 6) { "count is off" }
+        assert(result.results.size == 6) { "results size is off" }
     }
 }
